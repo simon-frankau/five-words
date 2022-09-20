a5703df5fb4617d9f55f550f26d64626c6182d80
Simon Frankau <sgf@arbitrary.name>
2022-09-20 10:48:03 +0100

    Implement algorithm

--- src/main.rs
+++ src/main.rs
@@ -32,16 +32,66 @@ fn to_bitmask(w: &str) -> u32 {
 }
 
 // Build a list of bitmasks, with the words associated with each mask.
-fn build_word_list<'a>(ws: Vec<String>) -> Vec<(u32, Vec<String>)> {
+fn build_word_list<'a>(ws: Vec<String>) -> HashMap<u32, Vec<String>> {
     let mut map = HashMap::new();
     for w in ws.into_iter() {
 	let mask = to_bitmask(&w);
 	let entry = map.entry(mask).or_insert_with(|| Vec::new());
 	entry.push(w);
     }
-    let mut v: Vec<(u32, Vec<String>)> = map.into_iter().collect::<Vec<_>>();
-    v.sort_unstable();
-    v
+    map
+}
+
+// Given a bitmask of used letters and a range of masks representing
+// words, fill in a vector with those masks that are compatible.
+fn candidates(used: u32, candidates: &[u32], output: &mut Vec<u32>) {
+    output.clear();
+    for candidate in candidates.iter() {
+	if candidate & used == 0 {
+	    output.push(*candidate);
+	}
+    }
+}
+
+fn solve(words: &[u32]) -> Vec<[u32; 5]> {
+    let mut words2 = Vec::with_capacity(words.len());
+    let mut words3 = Vec::with_capacity(words.len());
+    let mut words4 = Vec::with_capacity(words.len());
+    let mut words5 = Vec::with_capacity(words.len());
+
+    let mut res = Vec::new();
+    for (idx, word) in words.iter().enumerate() {
+	candidates(*word, &words[idx + 1..], &mut words2);
+	for (idx2, word2) in words2.iter().enumerate() {
+	    candidates(*word2, &words2[idx2 + 1..], &mut words3);
+	    for (idx3, word3) in words3.iter().enumerate() {
+		candidates(*word3, &words3[idx3 + 1..], &mut words4);
+		for (idx4, word4) in words4.iter().enumerate() {
+		    candidates(*word4, &words4[idx4 + 1..], &mut words5);
+		    for word5 in words5.iter() {
+			res.push([*word, *word2, *word3, *word4, *word5]);
+		    }
+		}
+	    }
+	}
+    }
+    res
+}
+
+// Print all the word combinations associated with a specific bitmask
+// solution.
+fn print_solution(solution: &[u32; 5], map: &HashMap<u32, Vec<String>>) {
+    for w1 in map.get(&solution[0]).unwrap().iter() {
+	for w2 in map.get(&solution[1]).unwrap().iter() {
+	    for w3 in map.get(&solution[2]).unwrap().iter() {
+		for w4 in map.get(&solution[3]).unwrap().iter() {
+		    for w5 in map.get(&solution[4]).unwrap().iter() {
+			println!("{}, {}, {}, {}, {}", w1, w2, w3, w4, w5);
+		    }
+		}
+	    }
+	}
+    }
 }
 
 fn main() {
@@ -53,4 +103,10 @@ fn main() {
 
     let word_list = build_word_list(candidate_words);
     eprintln!("{:?}", word_list);
+
+    let mask_vec = word_list.keys().cloned().collect::<Vec<_>>();
+    let solutions = solve(&mask_vec);
+    for solution in solutions {
+	print_solution(&solution, &word_list);
+    }
 }
